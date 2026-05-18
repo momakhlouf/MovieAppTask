f875eaab97e38952a771071811b2b1b5900683f9
Mohamed Makhlouf <momakhlouf.a@gmail.com>
2026-05-18 18:13:18 +0300

    Initial Commit

---
+++ MovieAppTask/MovieAppTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppTaskApp.swift
+//  MovieAppTask
+//
+//  Created by Mohamed Makhlouf Ahmed on 18/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
